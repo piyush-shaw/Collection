c82d2c709b2e899031ff391f67890074239c178a
piyush-shaw <piyushshaw0000@gmail.com>
2023-02-06 16:54:33 +0530

    [Piyush] added collection class demo program

---
+++ CollectionDemo/CollectionDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using CollectionDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+       // Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to c# collection!");
+        //Demo.doListDemo();
+        //Demo.doStackDemo();
+        // Demo.doDictionaryDemo();
+        //Demo.doQueueDemo();
+        Demo.doSetDemo();
+        Console.ReadKey();
+    }
+}
